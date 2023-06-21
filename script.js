efc55dfc7ee136a03b92127dc539d1ffb32de9df
GSIDD <guptasiddhant29.2000@gmail.com>
2023-06-21 20:17:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const display = document.querySelector(".display")
+const buttons = document.querySelectorAll("button")
+
+// console.log(buttons)
+let output = ""
+
+const specialChars = ["AC","DEL","%","/","*","+","/","-"];
+
+const calculate = (btnvalue)=>{
+     if(btnvalue==="=" && output!==""){
+         output = eval(output.replace("%","/100"))
+     }
+     else if(btnvalue=="AC"){
+        output = ""
+     }
+     else if(btnvalue=="DEL"){
+        output = output.toString().slice(0,-1);
+     }
+     else{
+        if(output==="" && specialChars.includes(btnvalue)) return
+        output+=btnvalue
+     }
+
+     display.value = output
+}
+
+buttons.forEach((button) => {
+     button.addEventListener("click",e => calculate(e.target.dataset.value))
+    //  console.log(button.dataset.value)
+})
+
